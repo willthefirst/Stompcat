e3b00a9cb9346766c66c2e34ae131d284655c2d8
Will Moritz <will@crowdfavorite.com>
2014-03-04 13:41:22 -0700

    admin bar now visible

--- footer.php
+++ footer.php
@@ -5,5 +5,7 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 // NOTE: this file is here for compatibility reasons - active templates are in the footer/ dir
 
 cfct_footer();
+wp_footer();
+
 
 ?>
